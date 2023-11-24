89f2e17febfe3d37940d01f3a6dd64739fb74fbc
dsiedin <115187533+dsiedin@users.noreply.github.com>
2023-11-24 22:13:41 +0100

    Delete Time-Series-Forecasting-System directory

--- Time-Series-Forecasting-System/project/timeSeries/checkingTrend/trend.js
+++
@@ -1,7 +0,0 @@
-import checkingAverageLevelDifferences from "./averageLevelDifferences/averageLevelDifferences.js";
-import seriesCriterion from "./seriesCriterion/seriesCriterion.js";
-
-export default function checkTrend(timeSeries) {
-  checkingAverageLevelDifferences(timeSeries);
-  seriesCriterion(timeSeries);
-}
